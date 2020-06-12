d89ccb75559b21c9b591aa833f8c6540f82fbb6d
EternalNothingness <p.wintner@aon.at>
2020-06-12 20:58:53 +0200

    Chat-Server.py added (although it is - up to now - not a chat-server, but only a simple server which sends data back to clients)

---
+++ Chat-Server.py
@@ -0,0 +1,54 @@
+# title: chat-server program
+# author: EternalNothingness
+# created: 12.06.2020
+# last change: 12.06.2020
+
+########################### imports ###########################
+import socket
+import sys
+from threading import Thread
+from time import sleep
+
+########################### classes ###########################
+class Chat_Server(object):
+
+    # ------------------------- init -------------------------
+    def __init__(self, host='localhost', port=12345, server_charset='cp850', n=5):  # init variables with default values
+        self.host = host    # define host
+        self.port = port    # define port
+        self.server_charset = server_charset    # define character set
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
+
+        self.s.bind(self.host, self.port)   # Bind to the port
+        self.s.listen(n)    # How often the server will allow unaccepted connections before refusing new ones
+        # Server is now initialized and prepared for client connection
+
+    # ------------------------- handle_connection -------------------------
+    def handle_connection(self, c, src):
+        while True:
+            try:
+                data = c.recv(1024)
+                if data.decode(self.server_charset) == "":
+                    break
+                print("Got from client", src, ": %s" % data.decode(self.server_charset))
+                c.send(data)  # send data back
+            except:
+                print("Unexpected error:", sys.exc_info()[1])
+                break
+        c.close()  # Close the connection
+
+    # ------------------------- establish_connection -------------------------
+    def establish_connection(self):
+        conn, client_address = self.s.accept()  # Establish connection with client.
+        # return values: conn and address
+        # 'conn' is a new socket object, which can be used to send and receive data on the connection
+        # 'address' is the address of the socket on the other end of the connection
+        print('Got connection from', client_address)
+        Thread(args=(conn, client_address), target=self.handle_connection).start()
+        sleep(1)
+
+########################### main program ###########################
+oChat_Server = Chat_Server()
+
+while True:
+    oChat_Server.establish_connection()
